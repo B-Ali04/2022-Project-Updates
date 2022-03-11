15030bd253413c7e523dd6e9f5feeca9e580f26e
B-Ali04 <86619743+B-Ali04@users.noreply.github.com>
2022-03-11 13:22:09 -0500

    Update students_repeating_class.sql

--- students_repeating_class.sql
+++ students_repeating_class.sql
@@ -1,14 +1,17 @@
 select
+
     SPRIDEN_LAST_NAME Last_Name,
     SPRIDEN_FIRST_NAME First_Name,
     GORADID.GORADID_ADDITIONAL_ID SUID,
     SFRSTCR.SFRSTCR_GRDE_CODE Letter_Grade,
-    shrtckn_subj_code, shrtckn_crse_numb,
+    shrtckn.shrtckn_subj_code, shrtckn.shrtckn_crse_numb,
     SSBSECT1.SSBSECT_SICAS_CAMP_COURSE_ID s1,
     SSBSECT.SSBSECT_SICAS_CAMP_COURSE_ID s2,
     ssbsect1.ssbsect_Term_code c1,
-    ssbsect.ssbsect_Term_code c2
-   
+    ssbsect.ssbsect_Term_code c2,
+    sfrstcr1.sfrstcr_grde_code
+    
+    
 from
     SPRIDEN SPRIDEN
 
@@ -29,17 +32,25 @@ from
     left outer join SHRTCKN SHRTCKN on SHRTCKN.SHRTCKN_PIDM = SFRSTCR.SFRSTCR_PIDM
         and SHRTCKN.SHRTCKN_TERM_CODE = SFRSTCR.SFRSTCR_TERM_CODE
         and SHRTCKN.SHRTCKN_CRN = SFRSTCR.SFRSTCR_CRN
-
+    
+    left outer join SSBSECT SSBSECT on SSBSECT.SSBSECT_CRN = SFRSTCR.SFRSTCR_CRN
+        and SSBSECT.SSBSECT_TERM_CODE = STVTERM.STVTERM_CODE
+        
     join STVMAJR STVMAJR on STVMAJR.STVMAJR_CODE = SGBSTDN.SGBSTDN_MAJR_CODE_1
     join STVDEGC STVDEGC on STVDEGC.STVDEGC_CODE = SGBSTDN.SGBSTDN_DEGC_CODE_1
     join STVCLAS STVCLAS on STVCLAS.STVCLAS_CODE = f_class_calc_fnc(SGBSTDN.SGBSTDN_PIDM,SGBSTDN.SGBSTDN_LEVL_CODE, STVTERM.STVTERM_CODE)
 
-    left outer join SSBSECT SSBSECT on SSBSECT.SSBSECT_CRN = SFRSTCR.SFRSTCR_CRN
-        and SSBSECT.SSBSECT_TERM_CODE = STVTERM.STVTERM_CODE
-/*
+
+
+
     join SFRSTCR SFRSTCR1 on SFRSTCR1.SFRSTCR_PIDM = SPRIDEN.SPRIDEN_PIDM
-         and SFRSTCR1.SFRSTCR_RSTS_CODE in ('RW', 'RE')
-*/
+        and SFRSTCR1.SFRSTCR_RSTS_CODE in ('RW', 'RE')
+        and SFRSTCR1.SFRSTCR_TERM_CODE = STVTERM.STVTERM_CODE
+
+    left outer join SHRTCKN SHRTCKN1 on SHRTCKN1.SHRTCKN_PIDM = SFRSTCR.SFRSTCR_PIDM
+        and SHRTCKN1.SHRTCKN_TERM_CODE = SFRSTCR.SFRSTCR_TERM_CODE
+        and SHRTCKN1.SHRTCKN_CRN = SFRSTCR.SFRSTCR_CRN
+    
     left outer join ssbsect ssbsect1 on ssbsect1.ssbsect_term_code != ssbsect.ssbsect_term_code and SSBSECT1.SSBSECT_SICAS_CAMP_COURSE_ID = SSBSECT.SSBSECT_SICAS_CAMP_COURSE_ID
          and SSBSECT1.SSBSECT_CRN = SFRSTCR.SFRSTCR_CRN
 where
@@ -54,9 +65,10 @@ where
         )   
         
         and SSBSECT1.SSBSECT_SICAS_CAMP_COURSE_ID = SSBSECT.SSBSECT_SICAS_CAMP_COURSE_ID
-        and sfrstcr_grde_code in ('F','IF')
-        --and (ssbsect1.ssbsect_term_code like '%%%%40' or (ssbsect1.ssbsect_term_code like '%%%%20'))
-        --and (ssbsect.ssbsect_term_code like '%%%%40' or (ssbsect.ssbsect_term_code like '%%%%20'))
+--        and sfrstcr.sfrstcr_grde_code in ('F','WF')
+        and (ssbsect1.ssbsect_term_code like '%%%%40' or (ssbsect1.ssbsect_term_code like '%%%%20'))
+        and (ssbsect.ssbsect_term_code like '%%%%40' or (ssbsect.ssbsect_term_code like '%%%%20'))
+        and SFRSTCR1.SFRSTCR_GRDE_CODE != SFRSTCR.SFRSTCR_GRDE_CODE 
 --$addfilter
 --$beginorder
 
